92358d127e18177e9abaf957c7412f9ef816b8d8
Aayush112000 <86760515+Aayush112000@users.noreply.github.com>
2023-08-01 09:48:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typifrom fastapi.templating import Jinja2Templatesng import Union
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,10 +6,23 @@ from pymongo import MongoClient
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-conn = MongoClient('mongodb+srv://aayushgajera2000:<password>@cluster0.z8ju6y2.mongodb.net/')
+conn = MongoClient('mongodb+srv://aayushgajera2000:sLhTGxrqv5Ma19oA@cluster0.z8ju6y2.mongodb.net/test')
 
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    docs = conn.notes.notes.find_one({})
+    newDocs=[]
+    for doc in docs:
+        newDocs.append({
+            "id": doc[_id],
+            "note": doc["note"]
+        } )
+    return templates.TemplateResponse("index.html",{"request":request, newDocs:newDocs  })
 
-
-
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str| None = None):
+    return {"item_id": item_id, "q":q}
